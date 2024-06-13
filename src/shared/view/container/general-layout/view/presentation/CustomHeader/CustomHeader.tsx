499ece41374ef04eb3eb0c13be97c096f945a2da
galihpermana29 <101629052+galihpermana292@users.noreply.github.com>
2024-06-14 00:55:20 +0700

    hotfix: adjustment adding location

--- src/shared/view/container/general-layout/view/presentation/CustomHeader/CustomHeader.tsx
+++ src/shared/view/container/general-layout/view/presentation/CustomHeader/CustomHeader.tsx
@@ -2,41 +2,45 @@ import { DownOutlined, UpOutlined } from '@ant-design/icons';
 import { Header } from 'antd/es/layout/layout';
 import { useState } from 'react';
 import useLogout from '../../../usecase/useLogout';
-import { IDetailUserData } from '@/shared/models/userServicesInterface';
+import {
+  ICreateUserPayloadRoot,
+  IDetailAdminUser,
+  IDetailVendorUser,
+} from '@/shared/models/userServicesInterface';
 
 interface ICustomHeader {
-	data: IDetailUserData;
+  data: ICreateUserPayloadRoot<IDetailAdminUser | IDetailVendorUser>;
 }
 export default function CustomHeader({ data }: ICustomHeader) {
-	const { name } = data;
-	const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { name } = data;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-	const handleLogout = useLogout();
+  const handleLogout = useLogout();
 
-	return (
-		<div className="border-b-[1px] border-ny-gray-200">
-			<Header className="flex justify-end relative bg-white">
-				<div
-					className="flex gap-[10px] items-center cursor-pointer"
-					onClick={() => setIsOpen(!isOpen)}>
-					<h1 className="capitalize">{name}</h1>
+  return (
+    <div className="border-b-[1px] border-ny-gray-200">
+      <Header className="flex justify-end relative bg-white">
+        <div
+          className="flex gap-[10px] items-center cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}>
+          <h1 className="capitalize">{name}</h1>
 
-					{isOpen ? (
-						<UpOutlined className="text-[12px]" />
-					) : (
-						<DownOutlined className="text-[12px]" />
-					)}
-				</div>
-				{isOpen && (
-					<div className="absolute top-[60px] z-[20] right-[2%] bg-white shadow-lg rounded-lg min-w-[150px] flex flex-col px-[10px]">
-						<p
-							className=" h-[50px] flex items-center cursor-pointer hover:text-blue-500"
-							onClick={handleLogout}>
-							Logout
-						</p>
-					</div>
-				)}
-			</Header>
-		</div>
-	);
+          {isOpen ? (
+            <UpOutlined className="text-[12px]" />
+          ) : (
+            <DownOutlined className="text-[12px]" />
+          )}
+        </div>
+        {isOpen && (
+          <div className="absolute top-[60px] z-[20] right-[2%] bg-white shadow-lg rounded-lg min-w-[150px] flex flex-col px-[10px]">
+            <p
+              className=" h-[50px] flex items-center cursor-pointer hover:text-blue-500"
+              onClick={handleLogout}>
+              Logout
+            </p>
+          </div>
+        )}
+      </Header>
+    </div>
+  );
 }
