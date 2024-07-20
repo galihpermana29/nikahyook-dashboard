5c7c5debb3c7e2314f71a33c21f1f66c39b17851
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-07-20 18:09:27 +0700

    fix: Add vendor id params

--- src/shared/repositories/reviewService.tsx
+++ src/shared/repositories/reviewService.tsx
@@ -8,9 +8,10 @@ class DashboardReviewService extends ApiClass {
 
   public async getAllReviews(query?: string): Promise<IAllReviewResponseRoot> {
     const token = JSON.parse(localStorage.getItem('token')!);
+    const id = JSON.parse(localStorage.getItem('admin')!);
 
     const { data } = await this.axiosInstance.get<IAllReviewResponseRoot>(
-      `/reviews${query ? `?${query}` : ''}`,
+      `/reviews${query ? `?vendor_id${id.user_id}&${query}` : ''}`,
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
